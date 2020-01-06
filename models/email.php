80b7069b912f141c26409670a45bd56d39aba3b8
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2020-01-06 13:27:12 -0500

    it was that dang semicolon. not sure if I'm angry or not. testing using a variable in the email

--- models/email.php
+++ models/email.php
@@ -1,14 +1,14 @@
 <?php
 require '../vendor/autoload.php';
 function email (){
-    $hello = 'hello';
+    $hello = 'HELLO';
     $email = new \SendGrid\Mail\Mail();
-$email->setFrom("arthur.k777@gmail.com", "Example User");
+$email->setFrom("noreply@noreply.com", "Example User");
 $email->setSubject("Sending with Twilio SendGrid is Fun");
 $email->addTo("arthur.k777@gmail.com", "Example User");
 $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+    "text/html", "<strong>and easy to do anywhere, $hello even with PHP</strong>"
 );
 $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 try {
