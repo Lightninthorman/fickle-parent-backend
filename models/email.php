f1d0b5049c2132e02cabd0f9b0402037241dade5
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2020-01-06 13:21:47 -0500

    getting errors when adding variables to email template. testing different options now.

--- models/email.php
+++ models/email.php
@@ -13,11 +13,11 @@ $email->addContent(
 $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 try {
     $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
+    print_r($response->statusCode() . "\n");
     print_r($response->headers());
-    print $response->body() . "\n";
+    print_r($response->body() . "\n");
 } catch (Exception $e) {
-    echo 'Caught exception: '.  $e->getMessage(). "\n";
+    print_r('Caught exception: '.  $e->getMessage(). "\n");
 }
 }
 
