d4390bc5b8f460fd66ca778620db20217d32da55
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2020-01-06 12:41:39 -0500

    testing to see if an email gets sent

--- models/email.php
+++ models/email.php
@@ -1,34 +1,34 @@
 <?php
 require '../vendor/autoload.php';
 function email (){
-//     $request_body = json_decode('{
-//   "personalizations": [
-//     {
-//       "to": [
-//         {
-//           "email": "arthur.k777@gmail.com"
-//         }
-//       ],
-//       "subject": "Hello World from the SendGrid PHP Library!"
-//     }
-//   ],
-//   "from": {
-//     "email": "arthur.k777@gmail.com"
-//   },
-//   "content": [
-//     {
-//       "type": "text/plain",
-//       "value": "Hello, Email!"
-//     }
-//   ]
-// }');
-//
-// $sg = new SendGrid(getenv('SENDGRID_API_KEY'));
-//
-// $response = $sg->client->mail()->send()->post($request_body);
-// echo $response->statusCode();
-// echo $response->body();
-// echo $response->headers();
+    $request_body = json_decode('{
+  "personalizations": [
+    {
+      "to": [
+        {
+          "email": "arthur.k777@gmail.com"
+        }
+      ],
+      "subject": "Hello World from the SendGrid PHP Library!"
+    }
+  ],
+  "from": {
+    "email": "arthur.k777@gmail.com"
+  },
+  "content": [
+    {
+      "type": "text/plain",
+      "value": "Hello, Email!"
+    }
+  ]
+}');
+
+$sg = new \SendGrid(getenv('SENDGRID_API_KEY'));
+
+$response = $sg->client->mail()->send()->post($request_body);
+echo $response->statusCode();
+echo $response->body();
+echo $response->headers();
 print_r('hello');
 }
 
