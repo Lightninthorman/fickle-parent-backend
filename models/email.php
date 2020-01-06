c6a7b8cf0dbcdc5fb06f71d9740a484109510dbe
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2020-01-06 15:19:49 -0500

    testing if I can show charts in an email

--- models/email.php
+++ models/email.php
@@ -2,42 +2,30 @@
 require '../vendor/autoload.php';
 function email (){
     $hello = 'HELLO';
+    $lowestAvg = 'Respect';
+    $improvement = '';
+    $rank=1;
+    $child ="Josie";
+    $user = "Dad";
+
+    if($rank > 1){
+        $improvement = "Don't rest though, you could always work on $lowestAvg";
+    }else{
+        $improvement = "A great way to improve would be to work on $lowestAvg";
+    }
     $email = new \SendGrid\Mail\Mail();
+
 $email->setFrom("arthur.k777@gmail.com", "Example User");
 $email->setSubject("Sending with Twilio SendGrid is Fun");
 $email->addTo("arthur.k777@gmail.com", "Example User");
 $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
     "text/html", "
-    <html lang='en' dir='ltr'>
-        <head>
-            <meta charset='utf-8'>
-            <script src='https://cdn.jsdelivr.net/npm/chart.js@2.8.0'></script>
-        </head>
-        <body>
-            <canvas id='myChart'></canvas>
-        <script>
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var chart = new Chart(ctx, {
-
-            type: 'line',
-
-
-            data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                datasets: [{
-                    label: 'My First dataset',
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: [0, 10, 5, 2, 20, 30, 45]
-                }]
-            },
-
-            options: {}
-            });
-        </script>
-        </body>
-    </html>
+    <p>Hi $child!</p>
+    <p>Currently you are my #$rank child with a score of  <strong>$score</strong>.</p>
+    <p>$improvement</p>
+    <p>Love,</p>
+    <p>$user</p>
     "
 );
 $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
