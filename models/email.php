a69cb28663f0ec3a847d1f7960e5aeb811e03e90
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2020-01-06 13:38:02 -0500

    testing if I can show charts in an email

--- models/email.php
+++ models/email.php
@@ -8,7 +8,38 @@ $email->setSubject("Sending with Twilio SendGrid is Fun");
 $email->addTo("arthur.k777@gmail.com", "Example User");
 $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
-    "text/html", "<strong>and easy to do anywhere, $hello even with PHP</strong>"
+    "text/html", "
+    <!DOCTYPE html>
+    <html lang='en' dir='ltr'>
+        <head>
+            <meta charset='utf-8'>
+            <script src='//https://cdn.jsdelivr.net/npm/chart.js@2.8.0'></script>
+        </head>
+        <body>
+            <canvas id='myChart'></canvas>
+        <script>
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+
+            type: 'line',
+
+
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'My First dataset',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45]
+                }]
+            },
+
+            options: {}
+            });
+        </script>
+        </body>
+    </html>
+    "
 );
 $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 try {
